a8a45ca8921c7333b9197885503c8803ba01a965
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-16 01:07:13 +0900

    get request and print http response

--- scripts/crawler/main.go
+++ scripts/crawler/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
-	resp, _ := http.Get("http://example.com/")
-	fmt.Println(resp)
+	resp, err := http.Get("https://www.linkedin.com/jobs/view/2800185827/?eBP=JOB_SEARCH_ORGANIC&recommendedFlavor=ACTIVELY_HIRING_COMPANY&refId=OLy91auxhev%2Bji3JoxTGMA%3D%3D&trackingId=ra4i8jiXL5tS4n2mI0oxVA%3D%3D&trk=flagship3_search_srp_jobs")
+	// resp, err := http.Get("http://example.com/")
+	if err != nil {
+		fmt.Println("HTTP Request Error!")
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Parse Error!")
+	}
+	fmt.Println(string(body))
 }
