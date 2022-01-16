d928cde4f8cba1b2a20ebe294f12820bc1cbdae4
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-16 22:24:37 +0900

    scraping jobs url

--- scripts/crawler/main.go
+++ scripts/crawler/main.go
@@ -8,25 +8,16 @@ import (
 )
 
 func main() {
-	// resp, err := http.Get("https://www.linkedin.com/jobs/search/?geoId=103366113&location=Vancouver%2C%20British%20Columbia%2C%20Canada")
-	// // resp, err := http.Get("http://example.com/")
-	// if err != nil {
-	// 	fmt.Println("HTTP Request Error!")
-	// }
-	// defer resp.Body.Close()
-	// body, err := io.ReadAll(resp.Body)
-	// if err != nil {
-	// 	fmt.Println("Parse Error!")
-	// }
-	// fmt.Println(string(body))
-
 	resp, err := soup.Get("https://www.linkedin.com/jobs/search?keywords=&location=Vancouver%2C%20British%20Columbia%2C%20Canada&geoId=103366113&trk=guest_homepage-basic_jobs-search-bar_search-submit&position=1&pageNum=0")
-
 	if err != nil {
 		os.Exit(1)
 	}
 	doc := soup.HTMLParse(resp)
-	links := doc.Find("div", "class", "base-card").Attrs()["data-search-id"]
-	fmt.Println(links)
-
+	links := doc.Find("ul", "class", "jobs-search__results-list").FindAll("li")
+	for _, link := range links {
+		url := link.Find("a", "class", "base-card__full-link")
+		if url.Pointer != nil {
+			fmt.Println(url.Attrs()["href"])
+		}
+	}
 }
