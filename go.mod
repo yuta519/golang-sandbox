21c486fee691a24848dd0b010983be0aa552ba8e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-17 13:49:21 +0900

    scraping company name

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/yuta519/golang-sandbox
 go 1.16
 
 require (
-	github.com/anaskhan96/soup v1.2.4 // indirect
+	github.com/anaskhan96/soup v1.2.4
 	github.com/urfave/cli/v2 v2.3.0
 )
--- scripts/crawler/main.go
+++ scripts/crawler/main.go
@@ -13,10 +13,20 @@ func main() {
 		os.Exit(1)
 	}
 	doc := soup.HTMLParse(resp)
+	fmt.Println(doc)
 	links := doc.Find("ul", "class", "jobs-search__results-list").FindAll("li")
 	for _, link := range links {
 		url := link.Find("a", "class", "base-card__full-link")
 		if url.Pointer != nil {
+			response, err := soup.Get(url.Attrs()["href"])
+			if err != nil {
+				return
+			}
+			parsed_html := soup.HTMLParse(response)
+			company_name := parsed_html.Find("a", "class", "topcard__org-name-link")
+			if company_name.Pointer != nil {
+				fmt.Println(company_name.Text())
+			}
 			fmt.Println(url.Attrs()["href"])
 		}
 	}
