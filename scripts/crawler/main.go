486b074b78a03ff6707e3c2589f41d0210d0f33c
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-24 22:33:32 +0900

    erase graphql

--- scripts/crawler/main.go
+++ scripts/crawler/main.go
@@ -11,15 +11,17 @@ import (
 
 func main() {
 	resp, err := soup.Get("https://www.linkedin.com/jobs/search?keywords=&location=Vancouver%2C%20British%20Columbia%2C%20Canada&geoId=103366113&trk=guest_homepage-basic_jobs-search-bar_search-submit&position=1&pageNum=0")
+
 	if err != nil {
 		os.Exit(1)
 	}
+
 	doc := soup.HTMLParse(resp)
 	links := doc.Find("ul", "class", "jobs-search__results-list").FindAll("li")
 	for _, link := range links {
 		url := link.Find("a", "class", "base-card__full-link")
 		if url.Pointer != nil {
-			fmt.Println(url.Attrs()["href"])
+			// fmt.Println(url.Attrs()["href"])
 			pkg.ExtractCompanyInfo(url.Attrs()["href"])
 			fmt.Println()
 		}
