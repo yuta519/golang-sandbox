9a77104fc2c727776257ec8d93990433e829d086
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-26 08:41:41 +0900

    fetch page info from database id

--- scripts/notion/main.go
+++ scripts/notion/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -16,6 +17,11 @@ func main() {
 	}
 
 	SECRET := "Bearer " + os.Getenv("NOTION_SECRET")
+	fmt.Println(fetch_database_id(SECRET))
+	fmt.Println(fetch_page_id(SECRET, "xxxxxxxxxxxxxxxxxxxxxxxxxx"))
+}
+
+func fetch_database_id(SECRET string) string {
 	req, _ := http.NewRequest("GET", "https://api.notion.com/v1/databases", nil)
 	req.Header.Add("Notion-Version", "2021-08-16")
 	req.Header.Set("Authorization", SECRET)
@@ -27,5 +33,23 @@ func main() {
 
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(body))
+	return string(body)
+}
+
+func fetch_page_id(SECRET string, db_id string) string {
+	endpoint := "https://api.notion.com/v1/databases/" + db_id + "/query"
+	fmt.Println(endpoint)
+	payload := strings.NewReader("{\"page_size\":100}")
+	req, _ := http.NewRequest("POST", endpoint, payload)
+	req.Header.Add("Notion-Version", "2021-08-16")
+	req.Header.Set("Authorization", SECRET)
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	return string(body)
 }
